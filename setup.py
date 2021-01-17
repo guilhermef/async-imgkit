7b6eaf973cf32d12bb8a2963a390aef36e804eae
Guilherme Souza <guilhermef@users.noreply.github.com>
2021-01-17 17:11:04 +0100

    Removing python 3.5

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
     url="https://github.com/guilhermef/async_imgkit",
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
